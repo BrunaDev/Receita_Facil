623a4f661227ed77f27493eaa926b708f4c9d813
BrunaDev <dev.bruna.magalhaes@gmail.com>
2023-12-28 23:25:43 -0300

    Update files

---
+++ src/components/video/index.js
@@ -0,0 +1,47 @@
+import { Text, TouchableOpacity, StyleSheet, SafeAreaView } from "react-native";
+
+import { Feather } from '@expo/vector-icons';
+import { WebView } from 'react-native-webview';
+
+export function VideoView({ handleClose, videoUrl }){
+    return(
+        <SafeAreaView>
+            <TouchableOpacity style={styles.backButton} onPress={handleClose}>
+                <Feather name="arrow-left" size={24} color="#FFF" />
+                <Text style={styles.backText}>Voltar</Text>
+            </TouchableOpacity>
+
+            <Text>testando</Text>
+            <WebView
+                style={styles.contentView}
+                videoUrl={{ uri: 'http://expo.dev' }}
+            />
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%'
+    },
+    backButton: {
+        width: '100%',
+        backgroundColor: '#4CBE6C',
+        height: 48,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingStart: 14
+    },
+    backText: {
+        color: "#FFF",
+        fontSize: 18,
+        fontWeight: '500',
+        marginLeft: 14
+    },
+    contentView: {
+        flex: 1,
+        width: '100%'
+    }
+})
